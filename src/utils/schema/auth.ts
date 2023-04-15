c0e59573331081b4b2e94756e394008f87d9f970
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2023-04-15 20:40:26 +0530

    Revert "update employer signup schema"
    This reverts commit 0398a9940147031f0f2e887e63ae51cb32469713.

--- src/utils/schema/auth.ts
+++ src/utils/schema/auth.ts
@@ -1,5 +1,4 @@
 import z from "zod"
-import validator from "validator"
 
 export const signUpSchema = z
 	.object({
@@ -46,17 +45,6 @@ export const signUpEmployerSchema = z
 			)
 			.min(8, "Password must be at least 8 characters in length"),
 		confirmPassword: z.string().min(1, "Password confirmation is required"),
-
-		companyName: z.string().min(1, "Company Name is required"),
-		companyPhone: z
-			.string()
-			.min(1, "Phone is required")
-			.refine((s) => validator.isMobilePhone(s, "en-IN"), { message: "Invalid Phone" }),
-		companyWebsite: z.string().min(1, "Website is required").url("Invalid URL"),
-		companyFoundedYear: z.string().regex(new RegExp("^([0-9]{4})?$"), "Invalid Year").optional(),
-		companySize: z.string().min(1, "Please select a value"),
-		companyAddress: z.string().min(1, "Address is required"),
-		companyDescription: z.string().max(256).optional(),
 	})
 	.refine((data) => data.password === data.confirmPassword, {
 		path: ["confirmPassword"],
