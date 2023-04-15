24e43c2960fb5e7007d9cd6f2e51562653f7f21d
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2023-04-15 17:14:59 +0530

    add employer register route

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -42,4 +42,37 @@ export const userRouter = createTRPCRouter({
 
 		return newUser
 	}),
+
+	registerEmployer: publicProcedure.input(signUpSchema).mutation(async ({ input, ctx }) => {
+		const { firstName, lastName, email, password } = input
+		const { prisma } = ctx
+
+		const user = await prisma.user.findFirst({
+			where: { email },
+		})
+
+		if (user) {
+			throw new TRPCError({
+				code: "CONFLICT",
+				message: "User already exists!",
+			})
+		}
+
+		const name = [firstName, lastName].join(" ")
+
+		const hashedPassword = await hash(password)
+
+		const newUser = await ctx.prisma.user.create({
+			data: {
+				firstName,
+				lastName,
+				name,
+				email,
+				password: hashedPassword,
+				passwordEnabled: true,
+			},
+		})
+
+		return newUser
+	}),
 })
