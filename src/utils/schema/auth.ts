c4e32f299bbdc94d491170d7f7b8deb270a4d0a1
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2023-04-15 17:14:40 +0530

    add employer signup schema

--- src/utils/schema/auth.ts
+++ src/utils/schema/auth.ts
@@ -28,5 +28,30 @@ export const logInSchema = z.object({
 	password: z.string().min(1, "Password is required"),
 })
 
+export const signUpEmployerSchema = z
+	.object({
+		firstName: z.string().min(1, "First Name is required"),
+		lastName: z.string().optional(),
+		email: z.string().min(1, "Email is required").email("Invalid email"),
+		password: z
+			.string()
+			.min(1, "Password is required")
+			.regex(new RegExp(".*[A-Z].*"), "Password must contain 1 uppercase character")
+			.regex(new RegExp(".*[a-z].*"), "Password must contain 1 lowercase character")
+			.regex(new RegExp(".*\\d.*"), "Password must contain 1 number")
+			.regex(
+				new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
+				"Password must contain 1 special character"
+			)
+			.min(8, "Password must be at least 8 characters in length"),
+		confirmPassword: z.string().min(1, "Password confirmation is required"),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		path: ["confirmPassword"],
+		message: "Passwords do not match",
+	})
+
 export type SignUpSchema = z.infer<typeof signUpSchema>
 export type LogInSchema = z.infer<typeof logInSchema>
+
+export type SignUpEmployerSchema = z.infer<typeof signUpEmployerSchema>
